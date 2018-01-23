86028c1c54a93d5c9d28683893e45e54b5796511
Fritz Otlinghaus <fritz@otlinghaus.it>
2018-01-23 17:02:52 +0100

    Add managesieve_port and defaults for managesieve_port and _host

--- templates/config.inc.php
+++ templates/config.inc.php
@@ -93,4 +93,5 @@ $config['mime_param_folding'] = '{{ roundcube_mime_param_folding }}';
 $config['mdn_requests'] = '{{ roundcube_mdn_requests }}';
 {% if 'managesieve' in roundcube_plugins %}
 $config['managesieve_host'] = '{{ roundcube_managesieve_host }}';
+$config['managesieve_port'] = '{{ roundcube_managesieve_port }}';
 {% endif %}
